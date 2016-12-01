6350c44d599ae55db5b9f23b936edcb5a89bd444
Alexander Kotliarov <slicer256@gmail.com>
2016-12-01 23:18:15 +0200

    simple sheets write/read code

--- src/SheetsApi/SheetsApi.jsx
+++ src/SheetsApi/SheetsApi.jsx
@@ -3,7 +3,7 @@
 export default class SheetsApi {
     config = {
         CLIENT_ID: '965160155749-7jofj31f6rvuh1iqv767hq2dldhui54h.apps.googleusercontent.com',
-        SCOPES: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+        SCOPES: ["https://www.googleapis.com/auth/spreadsheets.readonly"], // .readonly
         SPREADSHEET_ID: '1BczBIIBU_Lp3ddhJeeB-sbbF3opVNBYxUY2AS25eq0c',
         SPREADSHEET_ID_TEST: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
     }
@@ -13,16 +13,41 @@ export default class SheetsApi {
 
         var discoveryUrl = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
 
+
+
+        //console.log(gapi.client.sheets.spreadsheets.values);
+
         gapi.client.load(discoveryUrl).then(() => {
 
-            gapi.client.sheets.spreadsheets.values.get({
+            if (!gapi.client.sheets) {
+                alert('No API loaded');
+                return;
+            }
+
+            // WRITE
+            gapi.client.sheets.spreadsheets.values.update({
                 spreadsheetId: this.config.SPREADSHEET_ID,
-                range: 'Users!A:A',
+                range: 'Users!B3:B3',
+                majorDimension: "ROWS",
+                valueInputOption: 'RAW',
+                values: [
+                    ['WRITE VAL']
+                 ]
             }).then(response => {
-                console.log('RESPONSE: ', response.result.values);
+                console.log('RESPONSE: ', response.result);
             }, response => {
                 console.log('Error: ', response.result.error.message);
             });
+
+            // GET
+            // gapi.client.sheets.spreadsheets.values.get({
+            //     spreadsheetId: this.config.SPREADSHEET_ID,
+            //     range: 'Users!A:A',
+            // }).then(response => {
+            //     console.log('RESPONSE: ', response.result.values);
+            // }, response => {
+            //     console.log('Error: ', response.result.error.message);
+            // });
 
         });
     }
