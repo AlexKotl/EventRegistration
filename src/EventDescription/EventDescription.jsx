760514fc50acdb348f34647d725a4f1791791f12
Alexander Kotliarov <slicer256@gmail.com>
2016-12-03 23:17:21 +0200

    added cool icons describing technologies

--- src/EventDescription/EventDescription.jsx
+++ src/EventDescription/EventDescription.jsx
@@ -15,12 +15,53 @@ export default class EventDescription extends React.Component {
                 <p/>Never before signing up to event was such simple, fast and user friendly!
                 <p/>SPI (Single Page Application) based on React make life easier - no page reload, no extra traffic, no delays. Just click and get result. That's just an awesomeness!
                 <p/>With connection of <b>Google Spreadsheets API</b> this app brings more clarity and simplicity of support.
+
+                <div className="logos">
+                    <div className="icon react" id="reactIcon"></div>
+                    <div className="mdl-tooltip mdl-tooltip--large" htmlFor="reactIcon">
+                        <strong>React</strong><br/>
+                        Yes, famous Facebook library under the hood!
+                    </div>
+
+                    <div className="icon sheets" id="sheetsIcon"></div>
+                    <div className="mdl-tooltip mdl-tooltip--large" htmlFor="sheetsIcon">
+                        <strong>Google Spreadsheets API</strong><br/>
+                        Google technologies connected
+                    </div>
+
+                    <div className="icon flux" id="fluxIcon"></div>
+                    <div className="mdl-tooltip mdl-tooltip--large" htmlFor="fluxIcon">
+                        <strong>FLUX</strong><br/>
+                        Modern data flow
+                    </div>
+
+                    <div className="icon sass" id="sassIcon"></div>
+                    <div className="mdl-tooltip mdl-tooltip--large" htmlFor="sassIcon">
+                        <strong>SASS</strong><br/>
+                        CSS prepocessor
+                    </div>
+
+                    <div className="icon responsive" id="responsiveIcon"></div>
+                    <div className="mdl-tooltip mdl-tooltip--large" htmlFor="responsiveIcon">
+                        <strong>Responsive</strong><br/>
+                        Yes, you can use this app on any device!
+                    </div>
+                </div>
+
+
                 <p/>Don't waste your time! Try all these cool features right now with this cool button:
 
                 <p/>
                 <button onClick={this.props.showDialog} className="mdl-button mdl-js-button mdl-button--raised mdl-button--accent">
                     Sign Up for Event!
                 </button>
+
+                <p/><br/>
+                <center>
+                    <small>
+                        Source code: <a href="https://github.com/Slicer256/EventRegistration" target="_blank">https://github.com/Slicer256/EventRegistration</a>
+                    </small>
+                </center>
             </div>
         )
     }
